f37fc21920a8d1192650d9c507a62f4c5badf869
kiran kumar <kirandchennai@gmail.com>
2022-06-17 19:42:30 +0530

    forloop

---
+++ doo.js
@@ -0,0 +1,30 @@
+let a = process.argv[2]
+// for(let i=0;i<=a;i++){
+//   console.log(i)
+// }
+// for(let i=0;i<=a;i++){
+//   if(i%2==0){
+//     console.log(i)
+//   }
+// }
+// for(let i=a;i>=0;i--){
+//   console.log(i)
+// }
+
+// let i=0
+// while(i<=a){
+//   console.log(i);
+//   i++
+// }
+//  let i=0
+//  while(i<=a){
+//   if(i%2==0){
+//     console.log(i)
+//   }
+//   i++
+//  }
+let i=a;
+while(i>=0){
+  console.log(i)
+  i--
+}
